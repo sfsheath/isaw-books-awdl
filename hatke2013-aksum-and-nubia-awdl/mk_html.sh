c953c86a0f312fa9c51fe012ff12a7be9662dd57
Sebastian Heath <sebastian.heath@gmail.com>
2013-02-26 14:15:56 -0500

    hatke2013 build

--- hatke2013-aksum-and-nubia-awdl/mk_html.sh
+++ hatke2013-aksum-and-nubia-awdl/mk_html.sh
@@ -1,3 +1,3 @@
-cp ../../isaw/hatke2013-aksum-and-nubia/hatke2013-aksum-and-nubia.html index.xhtml
+# cp ../../isaw/hatke2013-aksum-and-nubia/hatke2013-aksum-and-nubia.html index.xhtml
 cp ../../isaw/hatke2013-aksum-and-nubia/hatke_map*.png .
-# xsltproc --nonet amheida2html.xsl ../../isaw/hatke2013-aksum-and-nubia/hatke2013-aksum-and-nubia.html
+xsltproc --nonet hatke2xhtml.xsl ../../isaw/hatke2013-aksum-and-nubia/hatke2013-aksum-and-nubia.xhtml > index.xhtml
